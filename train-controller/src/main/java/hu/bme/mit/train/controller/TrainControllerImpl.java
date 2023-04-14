66a23842c35705766e756188c0f41e673d681bad
potyok <potyok.csaba@edu.bme.hu>
2023-04-14 13:51:39 +0200

    add implementation

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
+import java.lang.Thread;
 
 public class TrainControllerImpl implements TrainController {
 
@@ -8,16 +9,32 @@ public class TrainControllerImpl implements TrainController {
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
 
+	public TrainControllerImpl() {
+		new Thread(this::controllReferenceSpeed).start();
+	}
+
+	private void controllReferenceSpeed() {
+		while (true) {
+			try {
+				referenceSpeed += step;
+				enforceSpeedLimit();
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				break;
+			}
+		}
+	}
+
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
 			referenceSpeed = 0;
 		} else {
-		    if(referenceSpeed+step > 0) {
-                referenceSpeed += step;
-            } else {
-		        referenceSpeed = 0;
-            }
+			if (referenceSpeed + step > 0) {
+				referenceSpeed += step;
+			} else {
+				referenceSpeed = 0;
+			}
 		}
 
 		enforceSpeedLimit();
@@ -32,7 +49,7 @@ public class TrainControllerImpl implements TrainController {
 	public void setSpeedLimit(int speedLimit) {
 		this.speedLimit = speedLimit;
 		enforceSpeedLimit();
-		
+
 	}
 
 	private void enforceSpeedLimit() {
@@ -43,12 +60,12 @@ public class TrainControllerImpl implements TrainController {
 
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
+		this.step = joystickPosition;
 	}
 
 	@Override
-	public void pushEmergencyBreaking(){
-		setSpeedLimit(0);  
-	} 
+	public void pushEmergencyBreaking() {
+		setSpeedLimit(0);
+	}
 
 }
